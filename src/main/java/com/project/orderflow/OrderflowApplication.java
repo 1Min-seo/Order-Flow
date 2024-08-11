0bb4abc9cfdc809cdd496011da3194565846c431
Minseo Han <readex11@naver.com>
2024-08-11 15:50:17 +0900

    setting

--- src/main/java/com/project/orderflow/OrderflowApplication.java
+++ src/main/java/com/project/orderflow/OrderflowApplication.java
@@ -8,6 +8,7 @@ public class OrderflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderflowApplication.class, args);
+        System.out.println("start!!");
     }
 
 }
