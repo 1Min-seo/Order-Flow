51fe1f88a6fcf3e3b39adb87d80acb2506cdd3cf
qhdudedi <boyoung035@gmail.com>
2024-08-25 13:17:56 +0900

    feat: 메뉴 주문 환경 구현
    admin에서 등록한 Menu / Seat과 연결시키기 위함

---
+++ src/main/java/com/project/orderflow/customer/domain/Menu.java
@@ -0,0 +1,24 @@
+package com.project.orderflow.customer.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+}
