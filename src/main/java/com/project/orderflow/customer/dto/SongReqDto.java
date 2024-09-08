c180d2265663f750610f1f6758d20469199ba5ae
qhdudedi <boyoung035@gmail.com>
2024-09-09 01:03:18 +0900

    feat: 노래 신청 도메인 설계

---
+++ src/main/java/com/project/orderflow/customer/dto/SongReqDto.java
@@ -0,0 +1,27 @@
+package com.project.orderflow.customer.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.orderflow.customer.domain.enums.SongStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SongReqDto {
+
+    private String tableNumber;
+
+    private String title;
+
+    private String artist;
+
+    private SongStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime requestedAt;
+
+}
