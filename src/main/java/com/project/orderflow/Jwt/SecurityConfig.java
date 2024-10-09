27b98eb90327191287b0bd3f14a00d6f2c5a00b3
[leedongguk] <[ldg4077@nate.com]>
2024-10-09 17:45:45 +0900

    2024-10-08

--- src/main/java/com/project/orderflow/Jwt/SecurityConfig.java
+++ src/main/java/com/project/orderflow/Jwt/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())  // CSRF 비활성화
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/auth/email/send", "/api/auth/signUp/send","/api/login", "/api/text", "/api/seats/activate").permitAll();  // 로그인과 회원가입은 모두 허용
-                    auth.requestMatchers("/swagger-orderflow-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll();  // Swagger 경로 허용
+                    auth.requestMatchers("/swagger-orderflow-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll();   // Swagger 경로 허용
                     auth.anyRequest().authenticated();  // 그 외 모든 요청은 인증 필요
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));  // 세션 사용하지 않음
