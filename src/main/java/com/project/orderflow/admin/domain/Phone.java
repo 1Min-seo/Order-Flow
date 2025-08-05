ca3eca44b024110653860d6b75ab7ca5acd7d4e3
한민서 <73270553+1Min-seo@users.noreply.github.com>
2025-08-05 11:20:52 +0900

    Update Phone.java

--- src/main/java/com/project/orderflow/admin/domain/Phone.java
+++ src/main/java/com/project/orderflow/admin/domain/Phone.java
@@ -12,7 +12,7 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@Component // 이 애노테이션을 추가하여 스프링 빈으로 등록합니다.
+@Component 
 public class Phone {
     private  String phoneNumber;
     private  String rand;
